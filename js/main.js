ea09393112c158432253d9013525d8bd0ef6f347
omerbag-9 <omerbagprog@gmail.com>
2023-12-10 01:45:45 +0200

    add events to booking page + fix responsivity

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
+// first room checkin and checkout
 var datesForDisable = ["2023-12-6", "2023-12-5"]
+var input1 = document.getElementsByClassName("check1-2")[0];
+var input2 = document.getElementsByClassName("check1-2")[1];
 
-$("input").datepicker({
+
+$(input1).datepicker({
   format: 'yyyy-mm-dd',
   autoclose: true,
   weekStart: 1,
   calendarWeeks: true,
   todayHighlight: true,
   datesDisabled: datesForDisable,
+})
+$(input2).datepicker({
+  format: 'yyyy-mm-dd',
+  autoclose: true,
+  weekStart: 1,
+  calendarWeeks: true,
+  todayHighlight: true,
+  datesDisabled: datesForDisable,
+})
+
+
+
+
+
+var datesForDisable1 = ["2023-12-7", "2023-12-8"]
+
+var input1 = document.getElementById("checkin");
+$(input1).datepicker({
+  format: 'yyyy-mm-dd',
+  autoclose: true,
+  weekStart: 1,
+  calendarWeeks: true,
+  todayHighlight: true,
+  datesDisabled: datesForDisable1,
 })
