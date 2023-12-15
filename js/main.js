e6d1a5d919ab0c7cfddab1306a0c7b516315b3cb
omerbag-9 <omerbagprog@gmail.com>
2023-12-15 21:57:43 +0200

    change

--- js/main.js
+++ js/main.js
@@ -425,33 +425,6 @@ async function joinData() {
 
 
 
-// get and add review
-// let reviewList = [];
-
-// async function addReview() {
-//     let myReq = await fetch(`https://xhotel-backend.onrender.com/api/api/reviews/`)
-//     let Data = await myReq.json()
-//     reviewList = Data.reviews
-//     console.log(reviewList)
-//     displayReview()
-//     }
-
-//     addReview()
-
-//     function displayReview(){
-//         let temp = ""
-//         reviewList.forEach((element)=>{
-//             temp+=`<div class="testimonials-item bg-body-tertiary">
-//         <div class="testimonials-name">
-//        <h5>${element.user}</h5>
-//        </div>
-//        <div class="testimonials-desc">
-//         <p><i class="fa-solid fa-comment"></i>${element.comment}<i class="fa-solid fa-comment"></i></p>
-//        </div>
-//       </div>`
-//         })
-//         document.getElementById("myReview").innerHTML = temp
-//     }
 
 
 
@@ -490,6 +463,41 @@ async function joinData() {
 
 
 
+
+
+
+
+
+//dina
+
+
+// get and add review
+// let reviewList = [];
+
+// async function addReview() {
+//     let myReq = await fetch(`https://xhotel-backend.onrender.com/api/api/reviews/`)
+//     let Data = await myReq.json()
+//     reviewList = Data.reviews
+//     console.log(reviewList)
+//     displayReview()
+//     }
+
+//     addReview()
+
+//     function displayReview(){
+//         let temp = ""
+//         reviewList.forEach((element)=>{
+//             temp+=`<div class="testimonials-item bg-body-tertiary">
+//         <div class="testimonials-name">
+//        <h5>${element.user}</h5>
+//        </div>
+//        <div class="testimonials-desc">
+//         <p><i class="fa-solid fa-comment"></i>${element.comment}<i class="fa-solid fa-comment"></i></p>
+//        </div>
+//       </div>`
+//         })
+//         document.getElementById("myReview").innerHTML = temp
+//     }
 
 
 
