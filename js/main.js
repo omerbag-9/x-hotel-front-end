6f6ea6af1bfaf35c44838ddaf0f85d815d2a4acf
omerbag-9 <omerbagprog@gmail.com>
2023-12-15 20:16:05 +0200

    ZCz

--- js/main.js
+++ js/main.js
@@ -426,32 +426,32 @@
 
 
 // get and add review
-// let reviewList = [];
+let reviewList = [];
 
-// async function addReview() {
-//     let myReq = await fetch(`https://xhotel-backend.onrender.com/api/api/reviews/`)
-//     let Data = await myReq.json()
-//     reviewList = Data.reviews
-//     console.log(reviewList)
-//     displayReview()
-//     }
+async function addReview() {
+    let myReq = await fetch(`https://xhotel-backend.onrender.com/api/api/reviews/`)
+    let Data = await myReq.json()
+    reviewList = Data.reviews
+    console.log(reviewList)
+    displayReview()
+    }
 
-//     addReview()
+    addReview()
 
-//     function displayReview(){
-//         let temp = ""
-//         reviewList.forEach((element)=>{
-//             temp+=`<div class="testimonials-item bg-body-tertiary">
-//         <div class="testimonials-name">
-//        <h5>${element.user}</h5>
-//        </div>
-//        <div class="testimonials-desc">
-//         <p><i class="fa-solid fa-comment"></i>${element.comment}<i class="fa-solid fa-comment"></i></p>
-//        </div>
-//       </div>`
-//         })
-//         document.getElementById("myReview").innerHTML = temp
-//     }
+    function displayReview(){
+        let temp = ""
+        reviewList.forEach((element)=>{
+            temp+=`<div class="testimonials-item bg-body-tertiary">
+        <div class="testimonials-name">
+       <h5>${element.user}</h5>
+       </div>
+       <div class="testimonials-desc">
+        <p><i class="fa-solid fa-comment"></i>${element.comment}<i class="fa-solid fa-comment"></i></p>
+       </div>
+      </div>`
+        })
+        document.getElementById("myReview").innerHTML = temp
+    }
 
 
 
