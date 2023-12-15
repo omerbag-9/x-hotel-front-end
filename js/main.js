a76bdbadb536a3ba6428d14cda47223da63ea74e
omerbag-9 <omerbagprog@gmail.com>
2023-12-15 20:37:09 +0200

    c

--- js/main.js
+++ js/main.js
@@ -314,37 +314,37 @@
 
 
 // register
-// var userNameJoinInput = document.getElementById("username")
-// var firstNameJoinInput = document.getElementById("first_name")
-// var lastNameJoinInput = document.getElementById("last_name")
-// var countryJoinInput = document.getElementById("country")
-// var zipJoinInput = document.getElementById("zip")
-// var emailJoinInput = document.getElementById("email")
-// var passwordJoinInput = document.getElementById("password")
-// var hobbiesJoinInput = document.getElementById("hobbies")
+var userNameJoinInput = document.getElementById("username")
+var firstNameJoinInput = document.getElementById("first_name")
+var lastNameJoinInput = document.getElementById("last_name")
+var countryJoinInput = document.getElementById("country")
+var zipJoinInput = document.getElementById("zip")
+var emailJoinInput = document.getElementById("email")
+var passwordJoinInput = document.getElementById("password")
+var hobbiesJoinInput = document.getElementById("hobbies")
 
-// async function joinData() {
-//     var joinUserData = {
-//         username: userNameJoinInput.value,
-//         first_name:firstNameJoinInput.value,
-//         last_name:lastNameJoinInput.value,
-//         country:countryJoinInput.value,
-//         zip:zipJoinInput.value,
-//         email: emailJoinInput.value,
-//         password: passwordJoinInput.value,
-//         hobbies:hobbiesJoinInput.value
-//     }
-//     var data = await fetch(`https://xhotel-backend.onrender.com/api/register/`, {
-//         method: "post",
-//         credentials:'include',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(joinUserData)
-//     })
-//     let res = await data.json()
-//     console.log(res);
-// }
+async function joinData() {
+    var joinUserData = {
+        username: userNameJoinInput.value,
+        first_name:firstNameJoinInput.value,
+        last_name:lastNameJoinInput.value,
+        country:countryJoinInput.value,
+        zip:zipJoinInput.value,
+        email: emailJoinInput.value,
+        password: passwordJoinInput.value,
+        hobbies:hobbiesJoinInput.value
+    }
+    var data = await fetch(`https://xhotel-backend.onrender.com/api/register/`, {
+        method: "post",
+        credentials:'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(joinUserData)
+    })
+    let res = await data.json()
+    console.log(res);
+}
 
 
 // Make a GET request to obtain the CSRF token from the HTML
@@ -397,24 +397,24 @@
 
 
 // review post
-var userPostReview = document.getElementById("user")
-var postReviewMessage = document.getElementById("comment")
+// var userPostReview = document.getElementById("user")
+// var postReviewMessage = document.getElementById("comment")
 
-async function sendReview() {
-    var userSendReview = {
-        user: userPostReview.value,
-        comment: postReviewMessage.value,
-    }
-    var data = await fetch(`https://xhotel-backend.onrender.com/api/api/reviews/`, {
-        method: "post",
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userSendReview)
-    })
-    let res = await data.json()
-    console.log(res);
-}
+// async function sendReview() {
+//     var userSendReview = {
+//         user: userPostReview.value,
+//         comment: postReviewMessage.value,
+//     }
+//     var data = await fetch(`https://xhotel-backend.onrender.com/api/api/reviews/`, {
+//         method: "post",
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(userSendReview)
+//     })
+//     let res = await data.json()
+//     console.log(res);
+// }
 
 
 
