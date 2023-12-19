4f2264a18893cd7e718c10b860022c092a75fcec
omerbag-9 <omerbagprog@gmail.com>
2023-12-19 03:24:47 +0200

    a

---
+++ js/logout.js
@@ -0,0 +1,29 @@
+// // Function to log out the user
+// function logout() {
+//     // Send a request to the server to invalidate the token
+//     fetch('/api/logout', {
+//       method: 'POST',
+//       headers: {
+//         'Authorization': 'Bearer ' + localStorage.getItem('token'), // Include the token in the headers
+//         'Content-Type': 'application/json',
+//       },
+//       // You can include a body if needed (depends on your server-side implementation)
+//       // body: JSON.stringify({}),
+//     })
+//     .then(response => {
+//       if (response.ok) {
+//         // Remove the token from local storage or wherever it's stored
+//         localStorage.removeItem('token');
+        
+//         // Redirect or perform other actions after successful logout
+//         window.location.href = '/login'; // Redirect to login page, for example
+//       } else {
+//         // Handle errors, such as displaying an error message
+//         console.error('Logout failed');
+//       }
+//     })
+//     .catch(error => {
+//       console.error('Error during logout:', error);
+//     });
+//   }
+  
