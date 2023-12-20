ed0317c8c868ce4500e916d5dce416cb13f824fa
omerbag-9 <omerbagprog@gmail.com>
2023-12-20 19:50:47 +0200

    a

--- js/logout.js
+++ js/logout.js
@@ -1,29 +1,18 @@
 // Function to log out the user
 function logout() {
     // Send a request to the server to invalidate the token
-    fetch('/api/logout', {
+    fetch('http://127.0.0.1:8000/api/logout/', {
       method: 'POST',
       headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem('token'), // Include the token in the headers
+        // 'Authorization': 'Bearer ' + localStorage.getItem('token'), // Include the token in the headers
         'Content-Type': 'application/json',
       },
       // You can include a body if needed (depends on your server-side implementation)
       // body: JSON.stringify({}),
     })
-    .then(response => {
       if (response.ok) {
-        // Remove the token from local storage or wherever it's stored
-        localStorage.removeItem('token');
-        
         // Redirect or perform other actions after successful logout
         window.location.href = 'http://127.0.0.1:8000/x-hotel-front-end/sign-in.html'; // Redirect to login page, for example
-      } else {
-        // Handle errors, such as displaying an error message
-        console.error('Logout failed');
       }
-    })
-    .catch(error => {
-      console.error('Error during logout:', error);
-    });
   }
   
