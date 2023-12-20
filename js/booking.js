987629f37966d9131048249b93ec5a2122d32abf
omerbag-9 <omerbagprog@gmail.com>
2023-12-20 19:22:23 +0200

    a

---
+++ js/booking.js
@@ -0,0 +1,150 @@
+$(document).ready(function() {
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
+    let qntYears = 4;
+    let selectYear = $("#year");
+    let selectMonth = $("#month");
+    let selectDay = $("#day");
+    let currentYear = new Date().getFullYear();
+  
+    for (var y = 0; y < qntYears; y++) {
+      let date = new Date(currentYear);
+      let yearElem = document.createElement("option");
+      yearElem.value = currentYear
+      yearElem.textContent = currentYear;
+      selectYear.append(yearElem);
+      currentYear++;
+    }
+  
+    for (var m = 0; m < 12; m++) {
+      let month = monthNames[m];
+      let monthElem = document.createElement("option");
+      monthElem.value = m;
+      monthElem.textContent = month;
+      selectMonth.append(monthElem);
+    }
+  
+    var d = new Date();
+    var month = d.getMonth();
+    var year = d.getFullYear();
+    var day = d.getDate();
+  
+    selectYear.val(year);
+    selectYear.on("change", AdjustDays);
+    selectMonth.val(month);
+    selectMonth.on("change", AdjustDays);
+  
+    AdjustDays();
+    selectDay.val(day)
+  
+    function AdjustDays() {
+      var year = selectYear.val();
+      var month = parseInt(selectMonth.val()) + 1;
+      selectDay.empty();
+  
+      //get the last day, so the number of days in that month
+      var days = new Date(year, month, 0).getDate();
+  
+      //lets create the days of that month
+      for (var d = 1; d <= days; d++) {
+        var dayElem = document.createElement("option");
+        dayElem.value = d;
+        dayElem.textContent = d;
+        selectDay.append(dayElem);
+      }
+    }
+  });
+
+
+
+
+
+/////////////////////////////////////////////
+  $(document).ready(function() {
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
+    let qntYears = 4;
+    let selectYear = $("#year1");
+    let selectMonth = $("#month1");
+    let selectDay = $("#day1");
+    let currentYear = new Date().getFullYear();
+  
+    for (var y = 0; y < qntYears; y++) {
+      let date = new Date(currentYear);
+      let yearElem = document.createElement("option");
+      yearElem.value = currentYear
+      yearElem.textContent = currentYear;
+      selectYear.append(yearElem);
+      currentYear++;
+    }
+  
+    for (var m = 0; m < 12; m++) {
+      let month = monthNames[m];
+      let monthElem = document.createElement("option");
+      monthElem.value = m;
+      monthElem.textContent = month;
+      selectMonth.append(monthElem);
+    }
+  
+    var d = new Date();
+    var month = d.getMonth();
+    var year = d.getFullYear();
+    var day = d.getDate();
+  
+    selectYear.val(year);
+    selectYear.on("change", AdjustDays);
+    selectMonth.val(month);
+    selectMonth.on("change", AdjustDays);
+  
+    AdjustDays();
+    selectDay.val(day)
+  
+    function AdjustDays() {
+      var year = selectYear.val();
+      var month = parseInt(selectMonth.val()) + 1;
+      selectDay.empty();
+  
+      //get the last day, so the number of days in that month
+      var days = new Date(year, month, 0).getDate();
+  
+      //lets create the days of that month
+      for (var d = 1; d <= days; d++) {
+        var dayElem = document.createElement("option");
+        dayElem.value = d;
+        dayElem.textContent = d;
+        selectDay.append(dayElem);
+      }
+    }
+  });
+////////////////////////////////////////////////////////////////
+
+
+
+var checkinyear = document.getElementById("year")
+var checkinmonth = document.getElementById("month")
+var checkinday = document.getElementById("day")
+console.log(checkin)
+
+function book(){
+    var checkin = checkinyear.value+'-'+checkinmonth.value+'-'+checkinday.value
+
+    console.log(checkin)
+}
+
+async function book1() {
+    var userSendReview = {
+        name: userPostReview.value,
+        comment: postReviewMessage.value,
+    }
+    var data = await fetch(`http://127.0.0.1:8000/api/api/reviews/`, {
+        method: "post",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userSendReview)
+    })
+    let res = await data.json()
+    console.log(res);
+}
