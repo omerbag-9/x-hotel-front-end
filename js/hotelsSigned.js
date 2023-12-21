8bdd5d17028ce8f4199a46c65c498a6f80c77496
omerbag-9 <omerbagprog@gmail.com>
2023-12-21 17:19:46 +0200

    aa

--- js/hotelsSigned.js
+++ js/hotelsSigned.js
@@ -13,7 +13,7 @@ async function addHotel() {
     function displayHotel(){
         let temp = ""
         hotelsList.forEach((element)=>{
-            temp+=`<a href="./bookingSigned.html">
+            temp+=`<a href="./bookingSigned${element.name}.html">
             <div class="hotels-item"  >
               <img src="css/images/Rectangle 20.png" alt="" />
               <div class="item-overlay">
