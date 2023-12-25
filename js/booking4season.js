1d6f8da60cf043f3eb50e7260e6d06101533c3d7
omerbag-9 <omerbagprog@gmail.com>
2023-12-25 03:15:51 +0200

    a

--- js/booking4season.js
+++ js/booking4season.js
@@ -18,6 +18,26 @@ reserveShow.addEventListener('click',function(){
 
 
 
+var reserveCloseEvent = document.querySelector("#reservationEvent button.close")
+var reserveShowEvent = document.getElementById("btn-bookingEvent")
+var reservebookEvent = document.getElementById("btnbookEvent")
+var reserveContainerEvent = document.getElementById("reservationEvent")
+
+reserveCloseEvent.addEventListener('click',function(){
+  reserveContainerEvent.style.visibility = 'hidden'
+})
+reservebookEvent.addEventListener('click',function(){
+  reserveContainerEvent.style.visibility = 'hidden'
+})
+reserveShowEvent.addEventListener('click',function(){
+  reserveContainerEvent.style.visibility = 'visible'
+})
+
+
+
+
+
+
 
 
 
@@ -103,6 +123,58 @@ reserveShow.addEventListener('click',function(){
     
             if (!data.ok) {
               window.alert("the room is booked in this time")
+                throw new Error(`HTTP error! Status: ${data.status}`);
+            }
+    
+            let res = await data.json();
+            console.log(res);
+            // Handle the success response here if needed
+            
+        } catch (error) {
+            console.error('Error during booking:', error);
+            // You can handle different types of errors here
+            if (error instanceof TypeError) {
+                // Handle specific error types if needed
+            } else {
+                // Handle other errors
+            }
+    
+            // Display an error message to the user or perform other error handling actions
+        }
+    }
+
+
+
+    //event
+    var checkindateEvent = document.getElementById("checkindateEvent")
+    // var namebookingEvent = document.getElementById("bookingNameEvent")
+    var eventType = document.getElementById("event_type")
+    var event_id = document.getElementById("event_id")
+    
+
+
+      async function bookEvent() {
+
+        var userSendBookEvent = {
+        // name:namebookingEvent.value,
+        event_type:eventType.value,
+        event_id:event_id.value,
+        check_in: checkindateEvent.value,
+      };
+    
+        try {
+      
+            var data = await fetch(`http://127.0.0.1:8000/api/event/`, {
+                method: "post",
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userSendBookEvent),
+            });
+    
+            if (!data.ok) {
+              window.alert("the event is booked in this time")
                 throw new Error(`HTTP error! Status: ${data.status}`);
             }
     
